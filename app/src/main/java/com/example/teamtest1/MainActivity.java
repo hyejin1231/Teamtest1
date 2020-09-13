c292224b34c8d189163564581e84f735c3432441
hyejin1231 <phj981231@naver.com>
2020-09-13 17:20:03 +0900

    [혜진]
    MainActivity
    Main_xml
    Sub_xml
    Menu 파일 menu.xml
    
    drawable 사진

--- app/src/main/java/com/example/teamtest1/MainActivity.java
+++ app/src/main/java/com/example/teamtest1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.teamtest1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,12 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //안녕하세요 테스트
-        //두번쨰
-        // 세번째 테스트 ~
-        //네번째!
-        // 다섯번째
-        // 권 시작
-        // 하이하이
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu); // 타이틀바의 아이콘 3개를 위해서
+        return true;
+    }
+
 }
