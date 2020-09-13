468b99ed6e9f4aad9ecad61ce2371abfe4d59ffd
hong <happyhong28@naver.com>
2020-09-13 14:03:30 +0900

    안녕하세요 테스트2

--- app/src/main/java/com/example/teamtest1/MainActivity.java
+++ app/src/main/java/com/example/teamtest1/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //안녕하세요 테스트
+        //두번쨰
     }
 }
