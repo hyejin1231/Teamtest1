f989f6497ab0811dabf3bbd1e0da247123806b9f
hong <happyhong28@naver.com>
2020-09-13 17:18:53 +0900

    [다혜/0913]                     Login                     SignUp
                        activity_login.xml
                        activity_sign_up.xml

--- app/src/main/java/com/example/teamtest1/Login.java
+++ app/src/main/java/com/example/teamtest1/Login.java
@@ -2,7 +2,10 @@ package com.example.teamtest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_Login = findViewById(R.id.btn_Login);
+        Button btn_SignUp = findViewById(R.id.btn_SignUp);
+
+        //로그인 버튼
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //회원가입 창 연결
+        btn_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SignUpIntent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(SignUpIntent);
+            }
+        });
+
     }
 }
