5cc25453988c9b7cbf5e88be7fed682a9ff27b6d
GwonGwonGwon <scoci@gc.gachon.ac.kr>
2020-10-11 17:17:00 +0900

    권 : 채팅 집어넣고 구현 완료      도착지 Uid는 Product 구현하면서 안에 같이 받아 넣게 만들면 그거 그대로 데려오면 됨      지금 테스트 버전은 보내는 사람은 누구든 상관 없고      받는 사람은 tt@naver.com / 123456이니 테스트 빌드 굴릴때 확인 부탁드립니다      파일들 건들인거 조금 있으니 확인 부탁드립니다
         Comments -> user 에서 User로 변수명 통일 완료
         (20.10.11)
         + 추가 사항
          현재 메시지 창이 1개만 뜨는 현상 발생으로 수정 진행중.

---
+++ app/src/main/java/com/example/teamtest1/fragment_chat.java
@@ -0,0 +1,15 @@
+package com.example.teamtest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_chat extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_chat);
+
+    }
+}
